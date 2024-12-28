8598c81ffdba748a98fa2eb659fa59f140430b67
Emil Maulana <emilmaulana10gmail.com>
2024-12-28 17:24:01 +0700

    first commit

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -84,49 +84,23 @@
             <div class="main-sidebar sidebar-style-2">
                 <aside id="sidebar-wrapper">
                     <div class="sidebar-brand">
-                        <a href="/">SIAKAD</a>
+                        <a href="/">ABSENSI</a>
                     </div>
                     <div class="sidebar-brand sidebar-brand-sm">
-                        <a href="/">LMS</a>
+                        <a href="/"><i class="fas fa-building"></i></a>
                     </div>
                     <ul class="sidebar-menu">
                         <li class="menu-header">Dashboard</li>
                         <li class="{{ Request::is('dashboard') ? 'active' : '' }}">
                             <a href="/dashboard" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
                         </li>                        
-                        <li class="menu-header">DATA DASAR</li>
+                        <li class="menu-header">MENU</li>
                         <li class="">
-                            <a class="nav-link" href=""><i class="fas fa-users"></i> <span>Biodata Siswa</span></a>
+                            <a class="nav-link" href=""><i class="fas fa-users"></i> <span>Absensi</span></a>
                         </li> 
-                        {{-- <li class="{{ request()->routeIs('siswa.*') ? 'active' : '' }}">
-                            <a class="nav-link " href="{{ route('siswa.index') }}"><i class="fas fa-user-graduate"></i> <span>Data Siswa</span></a>
-                        </li>
-                        <li class="{{ request()->routeIs('jurusan.*') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('jurusan.index') }}"><i class="fas fa-building"></i> <span>Unit Akademik</span></a>
-                        </li>
-                        <li class="{{ request()->routeIs('ruangan.*') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('ruangan.index') }}"><i class="fas fa-chalkboard"></i> <span>Ruang Kelas</span></a>
-                        </li>
-                        <li class="{{ request()->routeIs('guru.*') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('guru.index') }}"><i class="fas fa-chalkboard-teacher"></i> <span>Data Guru</span></a>
-                        </li>
-                        <li class="menu-header">PRA PEMBELAJARAN</li>
-                        <li class="{{ request()->routeIs('mapel.*') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('mapel.index') }}"><i class="fas fa-book"></i> <!-- Ikon untuk Mata Pelajaran --><span>Mata Pelajaran</span></a>
-                        </li>
-                        <li class="{{ request()->routeIs('kurikulum.*') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('kurikulum.index') }}"><i class="fas fa-th-list"></i> <!-- Ikon untuk Kurikulum --><span>Kurikulum</span></a>
-                        </li>
-                        <li class="{{ request()->routeIs('kelas.*') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('kelas.index') }}"><i class="fas fa-chalkboard-teacher"></i> <!-- Ikon untuk Manajemen Kelas --><span>Manajemen Kelas</span></a>
-                        </li>                         --}}
-                        <li><a class="nav-link" href="blank.html"><i class="fas fa-user-plus"></i> <span>Registrasi</span></a></li>
-                        <li><a class="nav-link" href="blank.html"><i class="fas fa-book-open"></i> <span>Rencana Studi</span></a></li>
-                        <li class="menu-header">PEMBELAJARAN</li>
-                        <li><a class="nav-link" href="blank.html"><i class="fas fa-calendar-alt"></i> <span>Jadwal Pembelajaran</span></a></li>
-                        <li><a class="nav-link" href="blank.html"><i class="fas fa-graduation-cap"></i> <span>Hasil Studi</span></a></li>
-                        <li class="menu-header">PKL</li>
-                        <li><a class="nav-link" href="blank.html"><i class="fas fa-briefcase"></i> <span>Praktik Kerja Lapangan</span></a></li>
+                        <li class="menu-header">ADMIN</li>
+                        <li><a class="nav-link" href=""><i class="fas fa-calendar-alt"></i> <span>Laporan</span></a></li>
+                        
 
                     </ul>
                     <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
